e765d0ea47c7f4a38b2aa23889316285857a1468
Mak Fof <43025563+mf3129@users.noreply.github.com>
2019-08-28 18:17:12 -0400

    changed cubes from linear distro to SIN() distro

--- Sketch.cs
+++ Sketch.cs
@@ -9,22 +9,25 @@ public class Sketch : MonoBehaviour
 
     void Start(){
 
-        int totalCubes = 8; 
+        int totalCubes = 12; 
 
-        int totalDistance = 5; 
+        float totalDistance = 2.9f; //How close cubes get to the wall. 
 
+        //SIN() DISTRIBUTION
         for (int i = 0; i < totalCubes; i++) {
             
             float perc = i /(float)totalCubes; 
 
-            float x = perc * totalDistance; 
+            float sin = Mathf.Sin(perc * Mathf.PI/2);  
+
+            float x = 1.8f + sin * totalDistance; //Distance to the main square. 
             float y = 5.0f; 
             float z = 0.0f; 
 
             //Easiest way to establish instantiate prefabs
             var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity); //Quarterrnion identity means just he default rotation. Instantiate returns an object so casting it as a game object. 
-            newCube.GetComponent<CubeScript>().setSize(1.0f - perc); //Accesing Publice Function in CubeScript
-            newCube.GetComponent<CubeScript>().rotateSpeed = perc; // Random.value
+            newCube.GetComponent<CubeScript>().setSize(.5f * (1.0f - perc) ); //Accesing Publice Function in CubeScript // Dampening the size by .5
+            newCube.GetComponent<CubeScript>().rotateSpeed = .2f + perc * 4.0f; // Adding .2 so the initial instance of a cubes speed is not zero. 
 
         }
 
@@ -36,3 +39,24 @@ public class Sketch : MonoBehaviour
         
     }
 }
+
+
+
+
+/* 
+        //Linear DISTRO
+        for (int i = 0; i < totalCubes; i++) {
+            
+            float perc = i /(float)totalCubes; 
+
+            float x = perc * totalDistance; 
+            float y = 3.0f; 
+            float z = 0.0f; 
+
+            //Easiest way to establish instantiate prefabs
+            var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity); //Quarterrnion identity means just he default rotation. Instantiate returns an object so casting it as a game object. 
+            newCube.GetComponent<CubeScript>().setSize(1.0f - perc); //Accesing Publice Function in CubeScript
+            newCube.GetComponent<CubeScript>().rotateSpeed = 0; // Random.value perc
+
+        }
+*/
